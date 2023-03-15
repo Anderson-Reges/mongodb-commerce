49f49b6d2e9175210af0e9d749919d044fb52da3
Anderson-Reges <andersonreges76@gmail.com>
2023-03-15 18:16:34 -0300

    feat: remove 'cebola' in products that have it

---
+++ challenges/desafio19.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  {
+    ingredientes: {
+      $all: ["cebola"],
+    },
+  },
+  {
+    $pull: {
+      ingredientes: "cebola",
+    },
+  },
+);
+db.produtos.find(
+  {},
+  {
+    nome: 1,
+    ingredientes: 1,
+    _id: 0,
+  },
+);
